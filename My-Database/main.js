29101da8544fdf2bcd772e31b2f0cdd0eec9d030
shota <shota19830706@gmail.com>
2023-09-06 09:19:51 +0900

    メール、パスワードそれぞれのフォームとボタンにidを振り、それぞれの値を変数として取得しました

--- My-Database/main.js
+++ My-Database/main.js
@@ -1,3 +1,8 @@
 (() => {
-    
+  const $doc = document;
+  const $mail = $doc.getElementById("#js-mail");
+  const $pass = $doc.getElementById("#js-button");
+  const $button = $doc.getElementById("#js-button");
+
+  
 })();
