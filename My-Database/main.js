6b5b1c48241a3ddbcadde321e9bc7c223f756b72
shota <shota19830706@gmail.com>
2023-09-06 11:30:05 +0900

    フォームに入力した値をローカルストレージに保存できるようにしました

--- My-Database/main.js
+++ My-Database/main.js
@@ -1,8 +1,16 @@
 (() => {
   const $doc = document;
-  const $mail = $doc.getElementById("#js-mail");
-  const $pass = $doc.getElementById("#js-button");
-  const $button = $doc.getElementById("#js-button");
+  const $mail = $doc.getElementById("js-mail");
+  const $pass = $doc.getElementById("js-pass");
+  const $button = $doc.getElementById("js-button");
 
-  
+  $button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const mailValue = $mail.value;
+    const passValue = $pass.value;
+
+    localStorage.setItem("email", mailValue);
+    localStorage.setItem("password", passValue);
+  });
 })();
