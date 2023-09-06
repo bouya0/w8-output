77ac0009d9204f4a793ac87ec9fb1363052cb1ba
shota <shota19830706@gmail.com>
2023-09-06 12:12:38 +0900

    一度サインインを押すと次回以降フォームの中身が自動入力されるようにしました

--- My-Database/main.js
+++ My-Database/main.js
@@ -1,16 +1,24 @@
 (() => {
   const $doc = document;
-  const $mail = $doc.getElementById("js-mail");
-  const $pass = $doc.getElementById("js-pass");
+  const $email = $doc.getElementById("js-email");
+  const $password = $doc.getElementById("js-password");
   const $button = $doc.getElementById("js-button");
 
   $button.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const mailValue = $mail.value;
-    const passValue = $pass.value;
+    const emailValue = $email.value;
+    const passwordValue = $password.value;
 
-    localStorage.setItem("email", mailValue);
-    localStorage.setItem("password", passValue);
+    localStorage.setItem("email", emailValue);
+    localStorage.setItem("password", passwordValue);
   });
+
+  const savedEmail = localStorage.getItem("email");
+  const savedPassword = localStorage.getItem("password");
+
+  if (savedEmail && savedPassword) {
+    $email.value = savedEmail;
+    $password.value = savedPassword;
+  }
 })();
